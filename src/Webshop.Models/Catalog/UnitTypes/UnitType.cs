c6735aa3fd88a478cd066264933f166315576953
bverboven <bramverboven@hotmail.com>
2026-03-23 07:57:10 +0100

    normalized

--- src/Webshop.Models/Catalog/UnitTypes/UnitType.cs
+++ src/Webshop.Models/Catalog/UnitTypes/UnitType.cs
@@ -1,9 +1,10 @@
 using Regira.Entities.Models.Abstractions;
+using Regira.Normalizing;
 using System.ComponentModel.DataAnnotations;
 
 namespace Webshop.Models.Catalog.UnitTypes;
 
-public class UnitType : IEntityWithSerial, IHasCode, IHasTitle, IHasTimestamps
+public class UnitType : IEntityWithSerial, IHasCode, IHasTitle, IHasNormalizedContent, IHasTimestamps
 {
     public int Id { get; set; }
     [Required, MaxLength(16)]
@@ -12,4 +13,6 @@ public class UnitType : IEntityWithSerial, IHasCode, IHasTitle, IHasTimestamps
     public string? Title { get; set; }
     public DateTime Created { get; set; }
     public DateTime? LastModified { get; set; }
+    [MaxLength(128), Normalized(SourceProperties = [nameof(Title), nameof(Code)])]
+    public string? NormalizedContent { get; set; }
 }
